ff1eac580d4278dcb2dc7e66a37738d250d12fc3
nicosio2 <nico.gruendel@web.de>
2015-06-04 10:00:37 +0200

    toString

--- src/main/java/org/pixelgaffer/turnierserver/minesweeper/MinesweeperBuilderResponse.java
+++ src/main/java/org/pixelgaffer/turnierserver/minesweeper/MinesweeperBuilderResponse.java
@@ -1,8 +1,15 @@
 package org.pixelgaffer.turnierserver.minesweeper;
 
+import java.util.Arrays;
+
 public class MinesweeperBuilderResponse {
 	
 	public String output;
 	public Cell[][] field;
+	
+	@Override
+	public String toString() {
+		return "Output: " + output + ", field: " + Arrays.toString(field);
+	}
 	
 }
