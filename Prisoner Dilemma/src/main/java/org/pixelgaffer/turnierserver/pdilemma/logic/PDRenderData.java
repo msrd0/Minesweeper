36af3929441f9d31122b227500c66ccaa6f37da3
nicosio2 <nico.gruendel@web.de>
2015-06-28 13:24:08 +0200

    Es wird nun mehr an das Frontend geschickt

---
+++ Prisoner Dilemma/src/main/java/org/pixelgaffer/turnierserver/pdilemma/logic/PDRenderData.java
@@ -0,0 +1,12 @@
+package org.pixelgaffer.turnierserver.pdilemma.logic;
+
+import java.util.HashMap;
+
+@SuppressWarnings("all")
+public class PDRenderData {
+  public HashMap<String, Boolean> action;
+  
+  public HashMap<String, Integer> points;
+  
+  public HashMap<String, String> output;
+}
---
+++ Prisoner Dilemma/src/main/java/org/pixelgaffer/turnierserver/pdilemma/logic/PDRenderData.xtend
@@ -0,0 +1,9 @@
+package org.pixelgaffer.turnierserver.pdilemma.logic
+
+import java.util.HashMap
+
+class PDRenderData {
+	public var HashMap<String, Boolean> action
+	public var HashMap<String, Integer> points
+	public var HashMap<String, String> output
+}
